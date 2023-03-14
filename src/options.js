f358b849348bb6d5d7c9f4fd62723cd30cc2a521
sinchandasgupta <sinchandsgpt9@gmail.com>
2023-03-14 23:07:51 +0530

    add environment file
    Signed-off-by: sinchandasgupta <sinchandsgpt9@gmail.com>

--- src/options.js
+++ src/options.js
@@ -2,8 +2,8 @@ export const options = {
   method: "GET",
   headers: {
     // Your Rapid-API Key
-    "X-RapidAPI-Key": "",
+    "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
     // API-Host
-    "X-RapidAPI-Host": "",
+    "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
   },
 };
